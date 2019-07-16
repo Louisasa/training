3d00e2fdce03c13056c0ad4f199dd59326dc065d
Louisa Nash <Louisa.Nash@softwire.com>
2019-07-16 10:36:08 +0100

    Finished Part 1 of SB

--- SupportBank/supportbank.js
+++ SupportBank/supportbank.js
@@ -1,50 +1,78 @@
-// const readline = require('readline');
-//
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-//
-// var question = function(q) {
-//     return new Promise( (res, rej) => {
-//         rl.question( q, answer => {
-//             res(answer);
-// })
-// });
-// };
-//
-// async function question1() {
-//     let answer = await question('Would you like to turn off rules? Y/N ');
-//     if (answer === "yes") {
-//         await question2();
-//     }
-//     doRules();
-// }
-//
-// async function question2() {
-//     let answer1;
-//     while (answer1 != "Y" && answer1 != "N") {
-//         answer1 = await question('Would you like to turn on the fizz rule? Y/N ');
-//     }
-//     if (answer1 === "Y") {
-//         questionList.push(fizzRule);
-//     }
-//
-// }
-//
-// const questionList = [];
-//
-// question1();
+class Account {
+    constructor(name, balance) {
+        this.name = name;
+        this.balance = balance;
+    }
 
+    addToBalance(amount) {
+        this.balance += amount;
+    }
 
-const csv = require('csv-parser');
-const fs = require('fs');
+    removeFromBalance(amount) {
+        this.balance -= amount;
+    }
+}
 
-fs.createReadStream('Transactions2014.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-    console.log(row);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var question = function(q) {
+    return new Promise( (res, rej) => {
+        rl.question( q, answer => {
+            res(answer);
+})
+});
+};
+
+async function askQuestion() {
+    let answer = await question('List all or specific account? ');
+    if (answer.toLowerCase() === "all") {
+        readInCSV("");
+    } else {
+        readInCSV(answer);
+    }
+}
+
+async function readInCSV(name) {
+    const csv = require('csv-parser');
+    const fs = require('fs');
+
+    const accounts = {};
+
+    fs.createReadStream('Transactions2014.csv')
+        .pipe(csv())
+        .on('data', (row) => {
+        //console.log(row);
+        // date
+        // from
+        // to
+        // narrative
+        // amount
+        if (row["From"] in accounts) {
+        accounts[row["From"]].account.removeFromBalance(parseInt(row["Amount"]));
+    } else {
+        accounts[row["From"]] = {account: new Account(row["From"], parseInt(row["Amount"]))};
+    }
+    if (row["To"] in accounts) {
+        accounts[row["To"]].account.addToBalance(parseInt(row["Amount"]));
+    } else {
+        accounts[row["To"]] = {account: new Account(row["To"], parseInt(row["Amount"]))};
+    }
 })
 .on('end', () => {
-    console.log('CSV file successfully processed');
+    if (name.length < 1) {
+        for (accountName in accounts) {
+            console.log(accounts[accountName].account);
+        }
+    } else {
+        console.log(accounts[name]);
+    }
 });
+
+}
+
+askQuestion();
