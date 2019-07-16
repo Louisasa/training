583552f4fa4724efcd58543a060e22ebc86b0dea
Louisa Nash <Louisa.Nash@softwire.com>
2019-07-16 15:25:24 +0100

    Works for XML now too SB

--- SupportBank/supportbank.js
+++ SupportBank/supportbank.js
@@ -50,7 +50,7 @@ async function askForFilename() {
     let answer;
     do {
         answer = await question('Please input the name of your file: ');
-    }while (!answer.includes("json") && !answer.includes("csv"));
+    }while (!answer.includes("json") && !answer.includes("csv") && !answer.includes("xml"));
     return answer;
 }
 
@@ -72,7 +72,7 @@ function parseData(row, toAccount, fromAccount, accounts) {
 
     // check that user has account if not init
     // look up account, add transaction
-    logger.debug(row["From"] + " is paying " + row["To"]);
+    logger.debug(row[fromAccount] + " is paying " + row[toAccount]);
     logger.debug(row["Amount"]);
     logger.debug(row["Date"]);
     if (!(row[fromAccount] in accounts)) {
@@ -136,21 +136,51 @@ function readInJSON(filename) {
     return accounts;
 }
 
+async function readInXML(filename) {
+    var XmlParser = require("xmljs");
+    var fs = require("fs");
+
+
+    var p = new XmlParser({ strict: true });
+    var xml = fs.readFileSync(filename); // XML in the examples direct
+    let accountInfo = 0;
+    p.parseString(xml, function(err, xmlNode) {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        accountInfo = xmlNode.children["TransactionList"][0].children["SupportTransaction"];
+    });
+    let accounts = [];
+    for (let index = 0; index < accountInfo.length; index++) {
+        // date under attributes, Date
+        // narrative under children, description
+        // amount under children, value
+        // to under children parties, to
+        // from under children, parties, from
+        const tmpAccountInfo = accountInfo[index];
+        const tmp = {Date: tmpAccountInfo.attributes["Date"]["text"], Narrative: tmpAccountInfo.children["Description"][0]["text"], To: tmpAccountInfo.children["Parties"][0].children["To"][0]["text"], From: tmpAccountInfo.children["Parties"][0].children["From"][0]["text"], Amount: tmpAccountInfo.children["Value"][0]["text"]};
+        accounts = parseData(tmp, "To", "From", accounts);
+    }
+    return accounts;
+}
+
 async function readInAccounts(filename) {
     if (filename.includes("csv")) {
         return await readInCSV(filename);
-    } else {
+    } else if (filename.includes("json")) {
         return readInJSON(filename);
+    } else {
+        return await readInXML(filename);
     }
 }
 
 const logger = log4js.getLogger('debug');
 
-//
-async function dothething() {
+async function startProgram() {
     const filename = await askForFilename();
     const accounts = await readInAccounts(filename);
     const accountName = await askAboutAccountNameToOutput();
     outputAccounts(accountName, accounts);
 }
-dothething();
+startProgram();
