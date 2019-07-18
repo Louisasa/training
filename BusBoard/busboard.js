3bb3383a94e307724d9e9eac6b369b6c4fc4f4a9
Louisa Nash <Louisa.Nash@softwire.com>
2019-07-18 12:25:06 +0100

    provisional part 1 for BB

--- BusBoard/busboard.js
+++ BusBoard/busboard.js
@@ -1,6 +1,6 @@
 var request = require('request');
 const busCode = "490008660N";
-request('https://api.tfl.gov.uk/StopPoint/'+busCode+'?app_id=86384f92&app_key=76aef114aa28b5f462c50654c942969e', function (error, response, body) {
+request('https://api.tfl.gov.uk/StopPoint/'+busCode+'/Arrivals??app_id=86384f92&app_key=76aef114aa28b5f462c50654c942969e', function (error, response, body) {
     if (error) {
         console.log('error:', error); // Print the error if one occurred
     }
@@ -11,14 +11,30 @@ request('https://api.tfl.gov.uk/StopPoint/'+busCode+'?app_id=86384f92&app_key=76
     // destinations
     // time until they arrive in minutes
 
-    // todo: check mode is bus
-    console.log(typeof jsonString);
-    console.log(jsonString)
+    // returns the next 6 buses, need the next 5 (but not in order)
+    // lineName is the name
+    // also make sure modeName is bus
+    // timeToStation is in seconds
+    // destinationName is their destination
+    // route is towards? maybe idk
 
-    // number of modes is number of types of travel that go to the stop so one in this case (0 = bus)
-    // children are the buses that visit this stop (this case 218 and 88)
-    // can check modes here to double check they are buses
-    // 
+    const timeHashMap = {};
+    let timesToStation = [];
+    for (let index in jsonString) {
+        // ensure is a bus
+        if (jsonString[index].modeName === "bus") {
+            timeHashMap[jsonString[index].timeToStation] = jsonString[index];
+            timesToStation.push(jsonString[index].timeToStation);
+        }
+    }
+    
+    timesToStation.sort(function(a, b){return a-b});
+
+    // next five earliest
+    for (let index = 0; index < 5; index++) {
+        const nextBus = timeHashMap[timesToStation[index]];
+        console.log(nextBus.lineName + " bus towards " + nextBus.towards + " terminating at " + nextBus.destinationName + " is " + Math.floor(nextBus.timeToStation/60) + " minutes away.");
+    }
 });
 //'https://api.tfl.gov.uk/StopPoint/Mode/bus/Disruption?app_id=86384f92&app_key=76aef114aa28b5f462c50654c942969e'
 //https://api.tfl.gov.uk/StopPoint/490008660N?app_id=86384f92&app_key=76aef114aa28b5f462c50654c942969e
